9bccb00e97b0281cf3ddadbeca0419df65f1af9c
Colin Woodbury <colin@fosskers.ca>
2025-06-13 08:02:48 +0900

    [tests] selection parsing tests

--- tests/tests.lisp
+++ tests/tests.lisp
@@ -27,9 +27,44 @@
   more than
   two lines")))
 
+#+nil
+(define-test parsing-ideally-passes-but-doesnt
+  :parent parsing
+  ;; By the spec, that extra newline in between should be preserved.
+  (fail (p:parse #'f::entry "this has
+  an extra
+
+  blank line!")))
+
 (define-test parsing-files
   :parent parsing
   (let ((s (uiop:read-file-string #p"tests/data/basic.ftl")))
     (finish (f:parse s)))
   (let ((s (uiop:read-file-string #p"tests/data/aura.ftl")))
     (finish (f:parse s))))
+
+(define-test parsing-minor-things
+  :parent parsing
+  (is eq :photocount (p:parse #'f::dollared "$photoCount"))
+  (is eq :other (p:parse #'f::category "other"))
+  (is eq :few (p:parse #'f::branch-selection-term "[few]"))
+  (is equal "male" (p:parse #'f::branch-selection-term "[male]"))
+  (is = 1 (p:parse #'f::branch-selection-term "[1]"))
+  (is = 1.0 (p:parse #'f::branch-selection-term "[1.0]")))
+
+(define-test parsing-selections
+  :parent parsing
+  (let* ((def (f::make-branch :term :other
+                              :line (list "added " (f::make-variable :name :photocount) " new photos")
+                              :default t))
+         (sel (f::make-selection :input :photocount
+                                 :func nil
+                                 :branches (list (f::make-branch :term :one
+                                                                 :line (list "added a new photo")
+                                                                 :default nil)
+                                                 def)
+                                 :default def)))
+    (is equalp sel (p:parse #'f::selection "{$photoCount ->
+  [one] added a new photo
+ *[other] added {$photoCount} new photos
+}"))))
